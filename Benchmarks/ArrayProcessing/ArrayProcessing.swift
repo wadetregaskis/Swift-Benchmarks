61559a8a03725b4c33827d2fff1dab32bf50f30f
Wade Tregaskis <wadetregaskis@mac.com>
2023-11-07 19:19:16 -0800

    Now test five different dataset sizes (0B, 32 KiB, 512 KiB, 8 MiB, 512 MiB).

--- Benchmarks/ArrayProcessing/ArrayProcessing.swift
+++ Benchmarks/ArrayProcessing/ArrayProcessing.swift
@@ -35,8 +35,6 @@ struct Datas {
     }
 }
 
-var testData = Datas(size: 1 << 28)
-
 let benchmarks = {
     Benchmark.defaultConfiguration = .init(metrics: [.cpuTotal,
                                                      .wallClock,
@@ -48,52 +46,63 @@ let benchmarks = {
                                            maxDuration: .seconds(30),
                                            maxIterations: 100)
 
-    Benchmark("for-in loop") { benchmark in
-        for _ in benchmark.scaledIterations {
-            var result = 0
+    for (dataSize, dataSizeLabel) in [
+        // Have to provide bespoke labels, rather than using e.g. (1 << dataSize).formatted(.byteCount(style: .binary)), because said formatter is broken (it uses decimal labels, e.g. "kB", rather than binary ones).
+        (0, "Empty collection"),  // Empty array.  Looks at the baseline overhead.
+        (15, "32 KiB"),           // Fits into L1 cache.
+        (19, "512 KiB"),          // Probably doesn't fit into L1, but should fit into L2.
+        (23, "8 MiB"),            // Probably doesn't fit into L2, but should fit into L3 (if present).
+        (28, "512 MiB")           // Probably doesn't fit into any CPU caches.
+    ] {
+        var testData = Datas(size: 1 << dataSize)
 
-            for value in testData.next {
-                if 0 == value % 2 {
-                    let value = value.byteSwapped
+        Benchmark("[\(dataSizeLabel)] for-in loop") { benchmark in
+            for _ in benchmark.scaledIterations {
+                var result = 0
 
-                    if (value & 0xff00) >> 8 < value & 0xff {
-                        let value = value.leadingZeroBitCount
+                for value in testData.next {
+                    if 0 == value % 2 {
+                        let value = value.byteSwapped
 
-                        if Int.bitWidth - 8 >= value {
-                            result &+= value
+                        if (value & 0xff00) >> 8 < value & 0xff {
+                            let value = value.leadingZeroBitCount
+
+                            if Int.bitWidth - 8 >= value {
+                                result &+= value
+                            }
                         }
                     }
                 }
+
+                blackHole(result)
             }
-
-            blackHole(result)
         }
-    }
 
-    Benchmark("Filter, map, and reduce") { benchmark in
-        for _ in benchmark.scaledIterations {
-            blackHole(testData.next
-                .filter { 0 == $0 % 2 }
-                .map { $0.byteSwapped }
-                .filter { ($0 & 0xff00) >> 8 < $0 & 0xff }
-                .map { $0.leadingZeroBitCount }
-                .filter { Int.bitWidth - 8 >= $0 }
-                .reduce(into: 0, &+=))
+        Benchmark("[\(dataSizeLabel)] Filter, map, and reduce") { benchmark in
+            for _ in benchmark.scaledIterations {
+                blackHole(testData.next
+                    .filter { 0 == $0 % 2 }
+                    .map { $0.byteSwapped }
+                    .filter { ($0 & 0xff00) >> 8 < $0 & 0xff }
+                    .map { $0.leadingZeroBitCount }
+                    .filter { Int.bitWidth - 8 >= $0 }
+                    .reduce(into: 0, &+=))
+            }
         }
-    }
 
-    Benchmark("Filter, map, and reduce (lazily)") { benchmark in
-        for _ in benchmark.scaledIterations {
-            blackHole(testData.next
-                .lazy
-                .filter { 0 == $0 % 2 }
-                .map { $0.byteSwapped }
-                .filter { ($0 & 0xff00) >> 8 < $0 & 0xff }
-                .map { $0.leadingZeroBitCount }
-                .filter { Int.bitWidth - 8 >= $0 }
-                .reduce(into: 0) { (result, value) in
-                    result &+= value
-                })
+        Benchmark("[\(dataSizeLabel)] Filter, map, and reduce (lazily)") { benchmark in
+            for _ in benchmark.scaledIterations {
+                blackHole(testData.next
+                    .lazy
+                    .filter { 0 == $0 % 2 }
+                    .map { $0.byteSwapped }
+                    .filter { ($0 & 0xff00) >> 8 < $0 & 0xff }
+                    .map { $0.leadingZeroBitCount }
+                    .filter { Int.bitWidth - 8 >= $0 }
+                    .reduce(into: 0) { (result, value) in
+                        result &+= value
+                    })
+            }
         }
     }
 }
