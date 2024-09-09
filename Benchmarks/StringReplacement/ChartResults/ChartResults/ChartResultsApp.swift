d2ef8166f51a04ab7f3d172f00dcb2a346daec28
Wade Tregaskis <wadetregaskis@mac.com>
2024-09-09 11:22:12 -0700

    Added ChartResults, for charting StringReplacement results.
    Still a work in progress.

---
+++ Benchmarks/StringReplacement/ChartResults/ChartResults/ChartResultsApp.swift
@@ -0,0 +1,18 @@
+//
+//  ChartResultsApp.swift
+//  ChartResults
+//
+//  Created by Wade Tregaskis on 6/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChartResultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(minWidth: 400, minHeight: 250)
+        }
+    }
+}
