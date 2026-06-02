32cd6cdb3556d9161b6de405c6f72e0b52209ee5
Wade Tregaskis <wadetregaskis@mac.com>
2026-06-02 14:42:09 -0700

    Updated to the new name & URL for the Benchmark package.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "5.3.0")),
-        .package(url: "https://github.com/ordo-one/package-benchmark", .upToNextMajor(from: "1.11.2")),
+        .package(url: "https://github.com/ordo-one/benchmark", .upToNextMajor(from: "1.11.2")),
         .package(url: "https://github.com/oscbyspro/Numberick.git", branch: "main"),
         .package(url: "https://github.com/pointfreeco/swift-gen.git",  .upToNextMajor(from: "0.4.0")),
         .package(url: "https://github.com/wadetregaskis/FoundationExtensions.git", .upToNextMajor(from: "3.4.0")),
@@ -18,28 +18,28 @@ let package = Package(
         .executableTarget(
             name: "ArrayProcessing",
             dependencies: [
-                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "Benchmark", package: "benchmark"),
                 .product(name: "Gen", package: "swift-gen"),
             ],
             path: "Benchmarks/ArrayProcessing",
             plugins: [
-                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+                .plugin(name: "BenchmarkPlugin", package: "benchmark")
             ]
         ),
         .executableTarget(
             name: "Clocks",
             dependencies: [
-                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "Benchmark", package: "benchmark"),
             ],
             path: "Benchmarks/Clocks",
             plugins: [
-                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+                .plugin(name: "BenchmarkPlugin", package: "benchmark")
             ]
         ),
         .executableTarget(
             name: "StringReplacement",
             dependencies: [
-                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "Benchmark", package: "benchmark"),
                 .product(name: "Gen", package: "swift-gen"),
             ],
             path: "Benchmarks/StringReplacement",
@@ -48,13 +48,13 @@ let package = Package(
                 "Output data munging commands.md",
             ],
             plugins: [
-                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+                .plugin(name: "BenchmarkPlugin", package: "benchmark")
             ]
         ),
         .executableTarget(
             name: "Swap",
             dependencies: [
-                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "Benchmark", package: "benchmark"),
                 .product(name: "BigInt", package: "BigInt"),
                 .product(name: "NBKFlexibleWidthKit", package: "Numberick"),
             ],
@@ -62,19 +62,19 @@ let package = Package(
 //            swiftSettings: [.unsafeFlags(["-Xfrontend", "-internalize-at-link", "-Xlinker", "-x", "-Xlinker", "-dead_strip", "-lto=llvm-full", "-experimental-hermetic-seal-at-link"])],
 //            linkerSettings: [.unsafeFlags(["-Xfrontend", "-internalize-at-link", "-Xlinker", "-x", "-Xlinker", "-dead_strip", "-lto=llvm-full", "-experimental-hermetic-seal-at-link"])],
             plugins: [
-                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+                .plugin(name: "BenchmarkPlugin", package: "benchmark")
             ]
         ),
         .executableTarget(
             name: "URLSession",
             dependencies: [
-                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "Benchmark", package: "benchmark"),
                 .product(name: "Gen", package: "swift-gen"),
                 .product(name: "FoundationExtensions", package: "FoundationExtensions"),
             ],
             path: "Benchmarks/URLSession",
             plugins: [
-                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+                .plugin(name: "BenchmarkPlugin", package: "benchmark")
             ]
         )
     ]
